e91296be5a94cf68b983605f66fe7667fe7ccff2
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2020-05-29 10:40:17 +0530

    Created proper exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,4 @@
-import * as React from 'react'
-import styles from './styles.module.css'
+import BlogEditor from './components/BlogEditor'
 
-interface Props {
-  text: string
-}
-
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+export const Editor = BlogEditor
+export default BlogEditor
