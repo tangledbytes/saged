a993a2a2510defae26c64e41c602a052bbb8368f
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2020-05-31 07:13:07 +0530

    Updates

---
+++ precache-manifest.ad30c44d6e5ed366030300709b456190.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5531b583d1ffaca77f4e40166ab1a965",
+    "url": "/saged/index.html"
+  },
+  {
+    "revision": "fc5f16669de6de081a3c",
+    "url": "/saged/static/css/2.ae87030c.chunk.css"
+  },
+  {
+    "revision": "5fdd493832f50b7c506e",
+    "url": "/saged/static/css/main.929e71dc.chunk.css"
+  },
+  {
+    "revision": "fc5f16669de6de081a3c",
+    "url": "/saged/static/js/2.8534307d.chunk.js"
+  },
+  {
+    "revision": "b23a8a042e51dd51bbd0294055a0b702",
+    "url": "/saged/static/js/2.8534307d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fdd493832f50b7c506e",
+    "url": "/saged/static/js/main.dc6bc959.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/saged/static/js/main.dc6bc959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7146da1fdddec66590a8",
+    "url": "/saged/static/js/runtime-main.f38dc430.js"
+  },
+  {
+    "revision": "02705ada6ee8ba8885afc53e0fe7f9d8",
+    "url": "/saged/static/media/blockquote~KUbmhmdU.02705ada.svg"
+  },
+  {
+    "revision": "1396e979cdc8461ada7f66dd9eda0113",
+    "url": "/saged/static/media/bold~smduNppc.1396e979.svg"
+  },
+  {
+    "revision": "239fded46d4625ec8b39c81ea0f34969",
+    "url": "/saged/static/media/code~bnLStfXP.239fded4.svg"
+  },
+  {
+    "revision": "0d1155f562dedb9f4e22b565bcadefa9",
+    "url": "/saged/static/media/heading~QOqIzRab.0d1155f5.svg"
+  },
+  {
+    "revision": "d72dac674fbd927c12bbd4e6db2b6b23",
+    "url": "/saged/static/media/italic~bkFUWxpm.d72dac67.svg"
+  },
+  {
+    "revision": "91b80a13626e318e9cbfdd7c2a4c245f",
+    "url": "/saged/static/media/markdown~lbIWfMVw.91b80a13.svg"
+  },
+  {
+    "revision": "e9866d995da1695f36b6209062f60d77",
+    "url": "/saged/static/media/orderedlist~GrRCmwBQ.e9866d99.svg"
+  },
+  {
+    "revision": "c6e2430b8dad4d14ee78011ca988a433",
+    "url": "/saged/static/media/plus~YOwKNLmC.c6e2430b.svg"
+  },
+  {
+    "revision": "1b1e8de63f31f681b8317b3fdaa1ffc6",
+    "url": "/saged/static/media/subheading~GoYdSCEx.1b1e8de6.svg"
+  },
+  {
+    "revision": "e281fa6a5f6aaea81b22c9919ff30c8a",
+    "url": "/saged/static/media/underline~dQaKFara.e281fa6a.svg"
+  },
+  {
+    "revision": "e797468febc5aa74b89a97e599c7c227",
+    "url": "/saged/static/media/unorderedlist~bfbrgOYJ.e797468f.svg"
+  }
+]);
